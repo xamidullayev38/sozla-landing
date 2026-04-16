d991cca0f32112f4bb3ab816809b91d50ad7e82a
xamidullayev38 <xamidullayevsaidkamol@gmail.com>
2026-04-16 16:13:40 +0500

    fixed: bugs

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,6 +1,6 @@
 import { Routes, Route } from "react-router-dom";
-import Home from "@/pages/home/Home";
-import About from "@/pages/about?/About";
+import Home from "@/pages/Home";
+import About from "@/pages/About";
 
 export default function App() {
   return (
